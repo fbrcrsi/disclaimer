f59d36f9e8b4e771fd152c40eb0dc54b6459faa3
fbrcrsi <fbrcrsi@gmail.com>
2015-02-25 22:22:05 +0100

    1st up

---
+++ language/fr/disclaimer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package Disclaimer Extension
+* @copyright (c) 2014 david63
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* Translated and adapted by fbrcrsi - http://www.fbrcrsi.fr
+* for http://www.valeuropeen.fr
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'DISCLAIMER'				=> 'Clause de non-responsabilité:',
+	'DISCLAIMER_TEXT'			=> '<h3>Clause de non-responsabilité du forum</h3>
+		<p>Les opinions et commentaires inscrits dans les sujets du forum %1$s sont personnels et n’engagent que ceux qui les écrivent.<br />L’équipe du forum %1$s ne peut être tenu responsable ni des propos tenus par des tiers, ni du contenu des sites externes cités.</p>',
+));
+
+?>
